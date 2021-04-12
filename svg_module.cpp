0b567498abd1e42a3fbce4426e9798ee5a51f83d
u211-05 <u211-05@public.mpei.local>
2021-04-12 17:17:16 +0300

    ttt

--- svg_module.cpp
+++ svg_module.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 #include "histogram.h"
 #include "svg_module.h"
 using namespace std;
@@ -44,17 +45,21 @@ show_histogram_svg(const vector<size_t>& bins) {
     for (size_t x : bins) {
         if (x > max_bin)max_bin = x;
      }
-
+    double numbers_count = 0;
+    for (size_t bin : bins) numbers_count+=bin;
     bool scale_flag = false;
-    if (max_bin*BLOCK_WIDTH > IMAGE_WIDTH) scale_flag = true;
+    if (max_bin*BLOCK_WIDTH > IMAGE_WIDTH-2*TEXT_WIDTH-5) scale_flag = true;
     if(max_bin)
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     double top = 0;
     for (size_t bin : bins) {
         double bin_width = bin*BLOCK_WIDTH;
-        if(scale_flag) bin_width = (static_cast<double>(bin)/max_bin)* IMAGE_HEIGHT;
+        if(scale_flag) bin_width = (static_cast<double>(bin)/max_bin)* (IMAGE_WIDTH-2*TEXT_WIDTH-5);
         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
         svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"black", "#e3256b");
+        double proc = round(((bin/numbers_count)*100));
+        string proc_s = to_sring(proc)+ "%";
+        svg_text(TEXT_LEFT+bin_width+5, top+TEXT_BASELINE, proc_s);
         top += BIN_HEIGHT;
     }
     svg_end();
