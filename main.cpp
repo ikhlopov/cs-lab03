28bd3eee84c6bdbcb88c9c3ce71910340615bd5c
u211-05 <u211-05@public.mpei.local>
2021-04-26 16:53:57 +0300

    refact done! cURL working

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <curl/curl.h>
 #include "histogram.h"
 #include "svg_module.h"
+#include <sstream>
+#include <string>
 using namespace std;
 
 vector<double>
@@ -31,13 +33,49 @@ read_input(istream& in, bool prompt) {
     return data;
 }
 
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    auto data_size = item_size * item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<char*>(items), data_size);
+    return data_size;
+}
+
+Input
+download(const string& address) {
+    curl_global_init(CURL_GLOBAL_ALL);
+    stringstream buffer;
+
+
+    CURL* curl = curl_easy_init();
+        if(curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL,  address.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+            res = curl_easy_perform(curl);
+            if (res != CURLE_OK) {
+                cout << address<<endl;
+                cout << curl_easy_strerror(res);
+                exit(1);
+               }
+
+            curl_easy_cleanup(curl);
+    }
+
+    return read_input(buffer, false);
+}
+
 int main(int argc, char* argv[])
 {
-    curl_global_init(CURL_GLOBAL_ALL);
-    if (argc>1) for(int i=0; i<argc; i++) cout<<"argv["<<i<<"] = "<<argv[i]<<endl;
-    //Input data = read_input(cin, true);
+    Input input;
+    if (argc > 1) {
+        input = download(argv[1]);
+    } else {
+        input = read_input(cin, true);
+    }
 
-    //const auto bins = make_histogram(data);
+    const auto bins = make_histogram(input);
 
-    //show_histogram_svg(bins);
+    show_histogram_svg(bins);
 }
